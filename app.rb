a672328ad5f0f3579b9cd246a43cc11db3621e1e
Elias Ventura <eliasventura23@gmail.com>
2019-03-05 20:28:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,15 @@ module FormsLab
 
     post '/' do
       @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
     end
+
+    erb :show
 
   end
 end
